a72a6deb6ace0058cf20c19b1cee31210ed0117d
Nhut Hao <c520a1@gmail.com>
2024-08-23 08:50:16 +0700

    Week1 js CodingChallenge6 Bills Tips Totals

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 //BMI = mass / height ** 2 = mass / (height * height) (mass in kg
 //and height in meter).
 //Data 1:-
-/*console.log("Coding Challenge 1- Data 1");
+console.log("Coding Challenge 1- Data 1");
 let heightMark = 1.69;
 let weightMark = 78;
 let BMIMark = weightMark / (heightMark ** 2);
@@ -17,7 +17,7 @@ let BMIJohn = weightJohn / (heightJohn ** 2);
 markHigherBMI = BMIMark > BMIJohn;
 console.log("BMI Mark: " + BMIMark);
 console.log("BMI John: " + BMIJohn);
-console.log("Mark has a higher BMI than John: " + markHigherBMI);*/
+console.log("Mark has a higher BMI than John: " + markHigherBMI);
 
 //Coding Challenge 2
 //Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
@@ -42,7 +42,7 @@ else
 //There are two gymnastics teams, Dolphins and Koalas. They compete against each
 //other 3 times. The winner with the highest average score wins a trophy!
 //Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
-/*console.log("Coding Challenge 3 - Data 1")
+console.log("Coding Challenge 3 - Data 1")
 
 function calculateScore(score1, score2, score3) {
     let averageScore = (score1 + score2 + score3) / 3
@@ -59,9 +59,9 @@ if (dolphinAverage > koalaAverage && dolphinAverage >= 100) {
     console.log(`They draw!`);
 } else {
     console.log(`No one win!`);
-}*/
+}
 // Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
-/*console.log("Coding Challenge 3 - Data 2")
+console.log("Coding Challenge 3 - Data 2")
 dolphinAverage = Math.round(calculateScore(97, 112, 101))
 koalaAverage = Math.round(calculateScore(109, 95, 106))
 console.log(dolphinAverage, koalaAverage)
@@ -73,7 +73,7 @@ if (dolphinAverage > koalaAverage && dolphinAverage >= 100) {
     console.log(`They draw!`);
 } else {
     console.log(`No one win!`);
-}*/
+}
 
 //Coding Challenge #4
 //Steven wants to build a very simple tip calculator for whenever he goes eating in a
@@ -81,13 +81,13 @@ if (dolphinAverage > koalaAverage && dolphinAverage >= 100) {
 //300. If the value is different, the tip is 20%.
 
 //Data 1:Test for bill values 275, 40 and 430
-/*console.log("Coding Challenge 4- Data 1");
+console.log("Coding Challenge 4- Data 1");
 const bill = 275
 const tip1 = bill * 0.15
 const tip2 = bill * 0.2
 bill >= 50 && bill <= 300 ?
     console.log(`The bill was ${bill}, the tip was ${tip1}, and the total value ${bill+tip1}`) :
-    console.log(`The bill was ${bill}, the tip was ${tip2}, and the total value ${bill+tip2}`);*/
+    console.log(`The bill was ${bill}, the tip was ${tip2}, and the total value ${bill+tip2}`);
 
 //Coding Challenge 5
 //Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
@@ -101,7 +101,7 @@ bill >= 50 && bill <= 300 ?
 //console.log("Coding Challenge 5- Data 1");
 //dolphinAverage = Math.round(calculateScore(44, 23, 71))
 //koalaAverage = Math.round(calculateScore(23, 34, 49))
-/*console.log("Data 2");
+console.log("Data 2");
 dolphinAverage = Math.round(calculateScore(85, 54, 41))
 koalaAverage = Math.round(calculateScore(23, 34, 27))
 console.log(dolphinAverage, koalaAverage)
@@ -120,7 +120,7 @@ function checkWinner(dolphinAverage, koalaAverage) {
         return console.log(`No team win!`)
     }
 }
-checkWinner(dolphinAverage, koalaAverage)*/
+checkWinner(dolphinAverage, koalaAverage)
 
 //Coding Challenge 6
 //Steven is still building his tip calculator, using the same rules as before: Tip 15% of
